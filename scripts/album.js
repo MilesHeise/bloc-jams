9026881ae26869d5a2f7396f6e481fc301d00a34
Miles Heise <milesheise@gmail.com>
2017-08-03 22:09:20 -0400

    fixed jquery playpause

--- scripts/album.js
+++ scripts/album.js
@@ -123,7 +123,7 @@ $(document).ready(function() {
 
      var albums = [albumPicasso, albumMarconi, albumRemy];
      var index = 1;
-     albumImage.addEventListener("click", function(event) {
+     $albumImage.click(function(event) {
        setCurrentAlbum(albums[index]);
        index++;
        if (index == albums.length) {
