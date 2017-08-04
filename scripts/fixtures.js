b60956d521a29100af866bf624088f6977377d66
Miles Heise <milesheise@gmail.com>
2017-08-04 10:39:34 -0400

    next/previous buttons

---
+++ scripts/fixtures.js
@@ -0,0 +1,44 @@
+var albumPicasso = {
+     title: 'The Colors',
+     artist: 'Pablo Picasso',
+     label: 'Cubism',
+     year: '1881',
+     albumArtUrl: 'assets/images/album_covers/01.png',
+     songs: [
+         { title: 'Blue', duration: '4:26' },
+         { title: 'Green', duration: '3:14' },
+         { title: 'Red', duration: '5:01' },
+         { title: 'Pink', duration: '3:21'},
+         { title: 'Magenta', duration: '2:15'}
+     ]
+ };
+
+ var albumMarconi = {
+     title: 'The Telephone',
+     artist: 'Guglielmo Marconi',
+     label: 'EM',
+     year: '1909',
+     albumArtUrl: 'assets/images/album_covers/20.png',
+     songs: [
+         { title: 'Hello, Operator?', duration: '1:01' },
+         { title: 'Ring, ring, ring', duration: '5:01' },
+         { title: 'Fits in your pocket', duration: '3:21'},
+         { title: 'Can you hear me now?', duration: '3:14' },
+         { title: 'Wrong phone number', duration: '2:15'}
+     ]
+ };
+
+ var albumRemy = {
+     title: 'I am Your Dog',
+     artist: 'Remy the Dog',
+     label: 'Woof',
+     year: '2017',
+     albumArtUrl: 'assets/images/album_covers/19.png',
+     songs: [
+         { title: 'I will pee on that', duration: '1:05' },
+         { title: 'I do not know how to fetch', duration: '5:15' },
+         { title: 'Quietly farting', duration: '4:21'},
+         { title: 'How about we just sleep all the time?', duration: '6:14' },
+         { title: 'Growling at skateboarders', duration: '2:45'}
+     ]
+ };
